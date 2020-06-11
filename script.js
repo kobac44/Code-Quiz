ba2fe9f41b097f467b618d063b60fe494b3a9b7d
Unko <kobie.watkins@gmail.com>
2020-06-11 07:33:34 -0400

    edit and debugged js work in progress, not sure why line 26 is active, line 68 and 147 have yet to be debugged-not clear as to what those problems are

--- script.js
+++ script.js
@@ -14,41 +14,68 @@ const correctAnswerEl = document.querySelector("#answer");
 const quizContainer = document.querySelector("#quiz");
 const resultsContainer = document.querySelector("#results");
 const submitButtonEl = document.querySelector("#submit");
-const secondsLeft = questions.length * 15 + 1;
+const secondsLeft = 180;
+const storedScores = document.querySelector("scorelist");
+const backButton = document.querySelector("#go-back");
+const clearScores = document.querySelector("#clear-highscores");
+const highScoreList = document.querySelector("#highScores");
+const messageDiv = document.querySelector("#message");
 
-function setTime() {
+function setTimer() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
-    timer.textContent = "Timer " + secondsLeft;
+    timer.textContent = "Timer" + secondsLeft;
+
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
       alert("Out of Time");
-      questions = {};
+      questionsEnder;
     } else if (i === questions.length) {
       clearInterval(timerInterval);
     }
   }, 1000);
-  return score;
+  return;
+}
+function questionEnder() {
+  const scoreTag = document.createElement("h1");
+  const inputTag = document.createElement("input");
+  const submitButton = document.createElement("button");
+  score += secondsLeft * 0.1;
+  score = score.toFixed(2);
+  document.getElementById("question").textContent = "All Done!";
+  answerOne.remove();
+  answerTwo.remove();
+  answerThree.remove();
+  answerFour.remove();
+  document.body.children[1].appendChild(scoreTag);
+  document.getElementsByTagName("h1")[0].setAttribute("id", "score");
+  document.getElementById("score").textContent = "Your Score: " + score;
+  document.body.children[1].appendChild(inputTag);
+  document.getElementsByTagName("input")[0].setAttribute("id", "input-field");
+  submitButton.textContent = "Submit";
+  document.body.children[1].appendChild(submitButton);
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var highScoreText = new Object();
+    highScoreText.name = inputTag.value.trim();
+    highScoreText.newScore = score;
+    storeScores(highScoreText);
+    window.location.href = "highScores.html";
+  });
 }
 
 function questionSetter() {
-  answerChoiceA.hidden = false;
-  answerChoiceB.hidden = false;
-  answerChoiceC.hidden = false;
-  answerChoiceD.hidden = false;
-  document.getElementById("startQuizButton").hidden = true;
+  document.getElementById("startQuiz").hidden = true;
   if (i === questions.length) {
     questions();
   } else {
-    document.getElementById("question").textContent = questions[i]["title"];
-    document.getElementById("answerOne").textContent =
-      questions[i]["choices"][0];
-    document.getElementById("answerTwo").textContent =
-      questions[i]["choices"][1];
-    document.getElementById("answerThree").textContent =
-      questions[i]["choices"][2];
-    document.getElementById("answerFour").textContent =
-      questions[i]["choices"][3];
+    document.getElementById("question").textContent = questions[i]["questions"];
+    document.getElementById("answerB").textContent =
+      questions[i]["choicesA"][0];
+    document.getElementById("answerC").textContent =
+      questions[i]["choicesB"][1];
+    document.getElementById("answerB").textContent = questions[i]["choiceC"][2];
+    document.getElementById("answerC").textContent = questions[i]["choiceD"][3];
   }
 }
 
@@ -64,7 +91,7 @@ function storeScores(highScoreText) {
 }
 
 // create the questions
-const myQuestions = [
+const questions = [
   {
     question: "Lionel Richie played with?",
     answers: {
@@ -114,3 +141,14 @@ document.getElementById("startButton").addEventListener("click", setTime);
 document.getElementById("startButton").addEventListener("click", function () {
   messageDiv.textContent = "";
 });
+
+function storeScores(highScoreText) {
+  tempArray = JSON.parse(localStorage.getItem("scores"));
+  if (tempArray === null) {
+    scoreList.push(highScoreText);
+    localStorage.setItem("scores", JSON.stringify(scoreList));
+  } else {
+    tempArray.push(highScoreText);
+    localStorage.setItem("scores", JSON.stringify(tempArray));
+  }
+}
