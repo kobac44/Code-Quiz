4193c23bf23a530c4dd1e4d7865560dcf8c6bfc9
Unko <kobie.watkins@gmail.com>
2020-06-12 10:32:26 -0400

    more edits and functions in js matching html id's and to the js functions and syntax development in js

--- script.js
+++ script.js
@@ -23,46 +23,46 @@ let checkAnswerDisplay = document.createElement("p");
 // this checks if scoreArray already exists
 
 const question = [
-  {
-    question: "Lionel Richie played with?",
-    answers: {
-      choiceA: "Jackson Five",
-      choiceB: "Commodores",
-      choiceC: "Brothers Johnson",
-      choiceD: "Five Heartbeats",
+  
+    question `Lionel Richie played with?`,
+    answers [
+      A`Jackson Five`,
+      B`Commodores`,
+      C`Brothers Johnson`,
+      D`Five Heartbeats`,
+    ],
+    answerB,
+  
+  
+    question `Maurice White played with?`,
+    answers [
+      A `Jackson Five`,
+      B `Commodores`,
+      C `Earth, Wind & Fire`,
+      D `Five Heartbeats`,
+    ],
+    answerC,
+  
+  
+    question `Michael Jackson played with?`,
+    answers {
+      A `Pointer Sisters`,
+      B `Jackson Five`,
+      C `Earth, Wind & Fire`,
+      D `Five Heartbeats`,
     },
-    correctAnswer: choiceB,
-  },
-  {
-    question: "Maurice White played with?",
-    answers: {
-      choiceA: "Jackson Five",
-      choiceB: "Commodores",
-      choiceC: "Earth, Wind & Fire",
-      choiceD: "Five Heartbeats",
+    answerB,
+  
+  
+    question `Diana Ross played with?`,
+    answers {
+      A `Pointer Sisters`,
+      B `Jackson Five`,
+      C `The Supremes`,
+      D `Five Heartbeats`,
     },
-    correctAnswer: choiceC,
-  },
-  {
-    question: "Michael Jackson played with?",
-    answers: {
-      choiceA: "Pointer Sisters",
-      choiceB: "Jackson Five",
-      choiceC: "Earth, Wind & Fire",
-      choiceD: "Five Heartbeats",
-    },
-    correctAnswer: choiceB,
-  },
-  {
-    question: "Diana Ross played with?",
-    answers: {
-      choiceA: "Pointer Sisters",
-      choiceB: "Jackson Five",
-      choiceC: "The Supremes",
-      choiceD: "Five Heartbeats",
-    },
-    correctAnswer: choiceC,
-  },
+    answerC,
+  
 ];
 
 //TIMER FUNCTION
@@ -92,17 +92,80 @@ function stopTimer() {
   timer.textContent = 0;
 }
 
-//question, end game quiz
-//function that goes through the array, grab data from the question to build the elements on the page.
-// need to
-// create the questions
+//START QUIZ
+/*Description: Upon user clicking Start Button,  the instructions and start button will disappear, 
+the timer will begin to countdown, and the first question will display*/
 startButton.addEventListener("click", function (event) {
   event.preventDefault();
   //Start Timer
   timerReturn = startTimer();
   //Hide Quiz Header and Start Display
-  quizHeader.style.display = "none";
+  quizTitle.style.display = "none";
   startDisplay.style.display = "none";
   //Start Questions
   createQuestionDisplay();
 });
+
+//Q&A FUNCTION
+/*Function Description: Creates Question and Answer Area. The questions are displayed from the questions array, 
+then the answers that correlate to that question are displayed as a list of buttons,so the user can choose an answer.
+Once the answer is choosen the checkAnswer function determines if an answer is correct or incorrect.*/
+
+function createQuestionDisplay() {
+  let ul = document.getElementById("queA");
+  const currentQ = question[index];
+  let li = document.createElement("li");
+  const questionh1 = document.createElement("h1");
+  questionh1.textContent = currentQ.question;
+  console.log(currentQ); //Test
+  li.className = "my-2";
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
+  li.appendChild(questionh1);
+  ul.appendChild(li);
+  for (let i = 0; i < currentQ.answers.length; i++) {
+    var answerChoices = currentQ.answers[i];
+    var answersli = document.createElement("li");
+    var button = document.createElement("button");
+    button.setAttribute("value", answerChoices);
+    button.textContent = answerChoices;
+    button.className += "btn btn-primary my-2";
+    ul.appendChild(answersli);
+    answersli.appendChild(button);
+    button.onclick = checkAnswer;
+  }
+}
+//CHECK ANSWER FUNCTION
+/*Function Description: Determines if the answer the user provides matches the given correct answer 
+and returns "incorrect" or "correct". If incorrect,  in addition to display of "incorrect, 10 seconds are deducted 
+from the timer, if correct the answer is provided, the user is displayed "correct" and the quiz continues until the timer 
+displays 0 or all the questions are answered (correctly or incorrectly)*/
+
+function checkAnswer() {
+  if (this.value !== questions[index].correctAnswer) {
+    console.log(this.value);
+    checkAnswerDisplay.textContent = "Incorrect";
+    checkAnswerDisplay.style.textAlign = "left";
+    answerCheck.appendChild(checkAnswerDisplay);
+    secondsLeft = secondsLeft - 10; //Time deduction
+    console.log(secondsLeft); //Test
+    timer.textContent = secondsLeft;
+    if (secondsLeft <= 0) {
+      secondsLeft = 0;
+      endQuiz();
+    }
+  } else {
+    checkAnswerDisplay.textContent = "Correct";
+    checkAnswerDisplay.style.textAlign = "left";
+    answerCheck.appendChild(checkAnswerDisplay);
+  }
+  index++;
+  if (index === questions.length) {
+    endQuiz();
+  }
+  createQuestionDisplay();
+}
+
+//END QUIZ FUNCTION
+/*Function Description: */
