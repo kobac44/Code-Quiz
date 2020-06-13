0eea4937247094711fd0b7b87dc8dc4b3cdfbad6
Unko <kobie.watkins@gmail.com>
2020-06-12 23:54:28 -0400

    updated html, css, js to function more connectively

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const questionsEl = document.querySelector("#question");
 const correctAnswersEl = document.querySelector("#answers");
 let scoreArray = localStorage.getItem("highscore");
 const timer = document.querySelector("#time");
-const choiceA = document.querySelector("A");
-const choiceB = document.querySelector("B");
-const choiceC = document.querySelector("C");
-const choiceD = document.querySelector("D");
-const resultsContainer = document.querySelector("#results");
-const submitButtonEl = document.querySelector("#submit");
+const choiceA = document.querySelector(`A`);
+const choiceB = document.querySelector(`B`);
+const choiceC = document.querySelector(`C`);
+const choiceD = document.querySelector(`D`);
+const resultsContainer = document.querySelector(`#results`);
+const submitButtonEl = document.querySelector(`#submit`);
 let secondsLeft = 75;
-const storedScores = document.querySelector("scorelist");
-const clearScores = document.querySelector("#clear-highscores");
+const storedScores = document.querySelector(`scorelist`);
+const clearScores = document.querySelector(`#clear-highscores`);
 let highScore = [];
-const messageDiv = document.querySelector("#message");
+const messageDiv = document.querySelector(`#message`);
 let timerReturn = "";
 let index = 0;
 let user = "";
@@ -23,47 +23,55 @@ let checkAnswerDisplay = document.createElement("p");
 // this checks if scoreArray already exists
 
 const question = [
-  
-    question `Lionel Richie played with?`,
-    answers [
-      A`Jackson Five`,
-      B`Commodores`,
-      C`Brothers Johnson`,
-      D`Five Heartbeats`,
+  {
+    question: `Lionel Richie played with?`,
+    answers: [
+      `A. Jackson Five`,
+      `B. Commodores`,
+      `C. Brothers Johnson`,
+      `D. Five Heartbeats`,
     ],
-    answerB,
-  
-  
-    question `Maurice White played with?`,
-    answers [
-      A `Jackson Five`,
-      B `Commodores`,
-      C `Earth, Wind & Fire`,
-      D `Five Heartbeats`,
+
+    correctnAswer: `B`,
+  },
+  {
+    question: `Maurice White played with?`,
+    answers: [
+      `A. Jackson Five`,
+      `B. Commodores`,
+      `C. Earth, Wind & Fire`,
+      `D. Five Heartbeats`,
     ],
-    answerC,
-  
-  
-    question `Michael Jackson played with?`,
-    answers {
-      A `Pointer Sisters`,
-      B `Jackson Five`,
-      C `Earth, Wind & Fire`,
-      D `Five Heartbeats`,
-    },
-    answerB,
-  
-  
-    question `Diana Ross played with?`,
-    answers {
-      A `Pointer Sisters`,
-      B `Jackson Five`,
-      C `The Supremes`,
-      D `Five Heartbeats`,
-    },
-    answerC,
-  
+    correctAnswer: `C`,
+  },
+  {
+    question: `Michael Jackson played with?`,
+    answers: [
+      `A. Pointer Sisters`,
+      `B. Jackson Five`,
+      `C. Earth, Wind & Fire`,
+      `D. Five Heartbeats`,
+    ],
+    correctAnswer: `B`,
+  },
+  {
+    question: `Diana Ross played with?`,
+    answers: [
+      `A. Pointer Sisters`,
+      `B. Jackson Five`,
+      `C. The Supremes`,
+      `D. Five Heartbeats`,
+    ],
+    correctAnswer: `C`,
+  },
 ];
+
+// this checks if scoreArray already exists
+if (scoreArray) {
+  scoreArray = JSON.parse(scoreArray);
+} else {
+  scoreArray = [];
+}
 
 //TIMER FUNCTION
 function time() {
@@ -117,7 +125,7 @@ function createQuestionDisplay() {
   let li = document.createElement("li");
   const questionh1 = document.createElement("h1");
   questionh1.textContent = currentQ.question;
-  console.log(currentQ); //Test
+  //console.log(currentQ); //Test
   li.className = "my-2";
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
@@ -143,10 +151,10 @@ from the timer, if correct the answer is provided, the user is displayed "correc
 displays 0 or all the questions are answered (correctly or incorrectly)*/
 
 function checkAnswer() {
-  if (this.value !== questions[index].correctAnswer) {
-    console.log(this.value);
+  if (this.value !== question[index].correctAnswer) {
+    //console.log(this.value);
     checkAnswerDisplay.textContent = "Incorrect";
-    checkAnswerDisplay.style.textAlign = "left";
+    checkAnswerDisplay.style.textAlign = "center";
     answerCheck.appendChild(checkAnswerDisplay);
     secondsLeft = secondsLeft - 10; //Time deduction
     console.log(secondsLeft); //Test
@@ -157,11 +165,11 @@ function checkAnswer() {
     }
   } else {
     checkAnswerDisplay.textContent = "Correct";
-    checkAnswerDisplay.style.textAlign = "left";
+    checkAnswerDisplay.style.textAlign = "center";
     answerCheck.appendChild(checkAnswerDisplay);
   }
   index++;
-  if (index === questions.length) {
+  if (index === question.length) {
     endQuiz();
   }
   createQuestionDisplay();
